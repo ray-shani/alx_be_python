4965c7842fccaf18c2a5e5d1806f44f0c030e128
Ray Shani <raysainepushani@gmail.com>
2025-06-08 22:47:20 +0300

    Updates on the if else

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -15,13 +15,13 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter the item name to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"'{item}' has been added to the list.")
 
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("Enter the item name to remove: ")
+            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"'{item}' has been removed from the list.")
