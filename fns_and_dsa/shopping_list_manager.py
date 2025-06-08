caee131dfc3fee2849bf1f8a43668a1cd6149a5b
Ray Shani <raysainepushani@gmail.com>
2025-06-08 22:45:45 +0300

    Corrections on the shopping list

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,6 +1,6 @@
 def display_menu():
     """Prints the main menu for the Shopping List Manager."""
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
